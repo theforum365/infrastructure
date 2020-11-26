acd99048a8d8f39fdc0f0ec1463dc99f44ca060d
frap365 <frap@theforum365.com>
2020-11-25 19:33:37 -0800

    add elasticache config

---
+++ elasticache/index.ts
@@ -0,0 +1,29 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+
+const stackRef = new pulumi.StackReference("vpc.prod")
+const vpcId = stackRef.getOutput("vpcId")
+const vpcCidr = stackRef.getOutput("vpcCidr")
+const privateSubnets = stackRef.getOutput("privateSubnetIds")
+
+const security_group = new aws.ec2.SecurityGroup("theforum365-cache-securitygroup", {
+    vpcId: vpcId,
+    ingress: [{
+        protocol: "tcp", fromPort: 6379, toPort: 6379, cidrBlocks: [vpcCidr, "172.1.0.0/16"]
+    }]
+})
+
+const subnet_group = new aws.elasticache.SubnetGroup("theforum365-cache-subnetgroup",{
+    subnetIds: privateSubnets,
+})
+
+const redis = new aws.elasticache.Cluster("theforum365-cache", {
+    engine: "redis",
+    engineVersion: "5.0.6",
+    nodeType: "cache.t3.small",
+    numCacheNodes: 1,
+    parameterGroupName: "default.redis5.0",
+    port: 6379,
+    subnetGroupName: subnet_group.name,
+    availabilityZone: "eu-west-1b",
+})
--- vpc/index.ts
+++ vpc/index.ts
@@ -2,20 +2,28 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
-// this defines a valid VPC that can be used for EKS
-const vpc = new awsx.ec2.Vpc(`theforum365`, {
-    numberOfAvailabilityZones: 1,
-    numberOfNatGateways: 1,
-    cidrBlock: "172.16.0.0/24",
-    subnets: [
-        {type: "private", tags: { tier: "production" }},
-        {type: "public", tags: { tier: "production" }}
-    ],
-    tags: {
-        tier: "production",
-    }
-});
 
-export const vpcId = vpc.id
-export const privateSubnets = vpc.privateSubnetIds
-export const publicSubnets = vpc.publicSubnetIds
+async function main() {
+    const vpc = new awsx.ec2.Vpc(`theforum365`, {
+        numberOfAvailabilityZones: 2,
+        numberOfNatGateways: 0,
+        cidrBlock: "172.16.0.0/24",
+        subnets: [
+            {type: "private", tags: {Name: "theforum365-private", tier: "production"}},
+            {type: "public", tags: {Name: "theforum365-public", tier: "production"}}
+        ],
+        tags: {
+            tier: "production",
+            Name: "theforum365"
+        }
+    });
+
+    return {
+        vpcId: vpc.id,
+        publicSubnetIds: vpc.publicSubnetIds,
+        privateSubnetIds: vpc.privateSubnetIds,
+        vpcCidr:"172.16.0.0/24",
+    }
+}
+
+module.exports = main()
